a77f2285b19d3e1614a8aa3b692f1d3f2341b803
Teixeira202213 <davidteixeira202213@gmail.com>
2023-08-30 22:23:50 -0300

    ex02

---
+++ ex02/index.js
@@ -0,0 +1,11 @@
+// alert("Você vai aprender sobre JavaScript!");
+// confirm("Esta gostando de JS?")
+let nome = prompt("Qual seu nome?") // Pop up que pede para digitar nome
+
+alert(" Bem Vindo! " + nome);
+
+let n1 = Number(prompt("Digite um número :"))
+let n2 = Number(prompt("Digite um número :"))
+let soma = n1 + n2;
+
+alert(`${nome} a soma de ${n1} + ${n2} é ${soma}`)
